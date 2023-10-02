be7e74785c04584bf4254288fd9e01c552179033
Ty <tyrellcarlton@gmail.com>
2023-10-02 15:24:33 -0700

    added a search box and created filter

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "tachyons";
-import Cardlist from "./Cardlist";
+import App from "./App";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
-import { robots } from "./robots";
 
-ReactDOM.render(<Cardlist robots={robots} />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
--- src/robots.js
+++ src/robots.js
@@ -1,31 +1,31 @@
-export const robots = [
-  {
+export const robots =[{
     id: 1,
-    name: "Terry",
-    username: "BtmnBYND",
-    email: "ntbtmn@waynetech.com",
-  },
+    name:"Terry",
+    username:'BtmnBYND',
+    email: 'ntbtmn@waynetech.com'
+},
 
-  {
+{
     id: 2,
-    name: "Bruce",
-    username: "OGBtmn",
-    email: "darealbatman@waynetech.com",
-  },
+    name:"Bruce",
+    username:'OGBtmn',
+    email: 'darealbatman@waynetech.com'
+},
 
-  {
+{
     id: 3,
-    name: "Alfred",
-    username: "Penny-1",
-    email: "pworth@waynetech.com",
-  },
+    name:"Alfred",
+    username:'Penny-1',
+    email: 'pworth@waynetech.com'
+},
 
-  {
-    id: 4,
-    name: "Penni",
-    username: "Penny-2",
-    email: "pworthdaughter@waynetech.com",
-  },
-];
+{
+        id: 4,
+        name:"Penni",
+        username:'Penny-2',
+        email: 'pworthdaughter@waynetech.com'
+    }
 
-export default robots;
+]
+
+export default robots
